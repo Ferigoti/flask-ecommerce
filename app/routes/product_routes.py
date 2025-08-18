5fc8ae4b766f768145f1656598d1e8134fdb1b3b
João Ferigoti <ferigoti318@outlook.com>
2025-08-18 17:00:31 -0300

    att

---
+++ app/routes/product_routes.py
@@ -0,0 +1,71 @@
+from flask import Blueprint, request, jsonify
+from flask_login import login_required
+from ..models import Product, db
+
+product_bp = Blueprint('product', __name__, url_prefix='/api/products')
+
+@product_bp.route('/add', methods=["POST"])
+@login_required
+def add_product():
+    data = request.json
+    if 'name' in data and 'price' in data:
+        product = Product(name=data["name"], price=data["price"], description=data.get("description", ""))
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({"message": "Product added successfuly"})
+    return jsonify({"message": "Invalid product data"}), 400
+
+@product_bp.route('/delete/<int:product_id>', methods=["DELETE"])
+@login_required
+def delete_product(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        db.session.delete(product)
+        db.session.commit()
+        return jsonify({"message": "Product deleted successfuly"})
+    return jsonify({"message": "Product not found"}), 404
+
+@product_bp.route('/<int:product_id>', methods=["GET"])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify({
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "description": product.description
+        })
+    return jsonify({"message": "Product not found"}), 404
+
+@product_bp.route('/update/<int:product_id>', methods=["PUT"])
+@login_required
+def uptade_product(product_id):
+    product = Product.query.get(product_id)
+    if not product:
+        return jsonify({"message": "Product not found"}), 404
+    
+    data = request.json
+    if 'name' in data:
+        product.name = data['name']
+
+    if 'price' in data:
+        product.price = data['price']
+
+    if 'description' in data:
+        product.description = data['description']
+
+    db.session.commit()
+    return jsonify({'message': 'Product updated sucessfully'})
+
+@product_bp.route('/', methods=['GET'])
+def get_products():
+    products = Product.query.all()
+    product_list = []
+    for product in products:
+        product_data = {
+            "id": product.id,
+            "name": product.name,
+            "price": product.price
+        }
+        product_list.append(product_data)
+    return jsonify(product_list)
